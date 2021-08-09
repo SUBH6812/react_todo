44dcb09fd4619eb09d3dd98f229bed6b868467ff
SUBH6812 <79861234+SUBH6812@users.noreply.github.com>
2021-08-09 23:33:27 +0530

    lm

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// import Pact from "./pact/Pact";
 import Todo from "./todo/Todo";
-
-// import Todo1 from "./todo/Todo1";
-// import "./App.css";
- // import "./todo/Todo.css";
 
 
 
